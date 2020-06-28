592d8a6d654447d60d0d059987806c87c5c8c6b3
anthony-rk <anthonyrkowalski@gmail.com>
2020-06-28 12:42:10 -0700

    Update getWeateherData function

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
-const apiKey = prompt("What is your API Key?");
-
-const city = prompt("What city do you want to see the Weather for?");
-
-
+const apiKey = 'e403aee2404f8ccd850fb30fa2b1ef02';
 // URL (required), options (optional)
-fetch('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&APPID=' + apiKey, {mode: 'cors'})
-.then(function(response) {
-  return response.json();
-})
-.then(function(response) {
-  console.log(response);
-  console.log(response.weather[0].description);
-  console.log('Temp in Celcius is: ' + Math.round(kelvinToCelcius(response.main.temp))); 
-  console.log('Temp in Fahrenheit is: ' + Math.round(kelvinToFahrenheit(response.main.temp))); 
-});
+const getWeatherData = (city, apiKey) => {
+  const weatherGeneralDiv = document.getElementById('weatherGeneral');
+  const weatherTempDiv = document.getElementById('weatherTemp');
+
+  fetch('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&APPID=' + apiKey, {mode: 'cors'})
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(response) {
+    // console.log(response);
+    // console.log(response.weather[0].description);
+    // console.log('Temp in Celcius is: ' + Math.round(kelvinToCelcius(response.main.temp))); 
+    // console.log('Temp in Fahrenheit is: ' + Math.round(kelvinToFahrenheit(response.main.temp))); 
+    weatherGeneralDiv.innerHTML = response.weather[0].description;
+    weatherTempDiv.innerHTML = Math.round(kelvinToFahrenheit(response.main.temp));
+  });
+};
 
 // Conversions from Kelvin to C or F
 const kelvinToCelcius = (kelvinTemp) => {
@@ -25,3 +28,8 @@ const kelvinToFahrenheit = (kelvinTemp) => {
     let fahrenheitTemp = kelvinTemp * (9/5) - 459.67;
     return fahrenheitTemp;
 };
+
+
+// Run this on Form Submission, either do Fahrenheit or Celcius depending on Button
+getWeatherData('Ann Arbor', apiKey);
+
